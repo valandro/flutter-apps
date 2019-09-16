ea43d48b781f145da10e5c3bff91282f9aa5df72
Lucas <lvalandrorocha@hotmail.com>
2019-09-16 14:07:20 -0300

    Mi Card app

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,20 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.red,
-          body: Container(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 75.0, backgroundImage: AssetImage('img/me.jpeg')),
+              Text(
+                'Lucas Valandro',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'COMPUTER ENGINEERING STUDENT',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSans'),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+55 51 99353 5686',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.mail, color: Colors.teal),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'lvalandrorocha@hotmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
