4a7c957a9ccb8af0c46cce2a0136e11421841b18
Lucas <lvalandrorocha@hotmail.com>
2019-09-19 14:30:34 -0300

    Usign Card Widget instead of Row

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -33,51 +33,46 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontFamily: 'SourceSans'),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+55 51 99353 5686',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSans',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade200,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              Card(
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.mail, color: Colors.teal),
-                    SizedBox(
-                      width: 10.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+55 51 99353 5686',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
                     ),
-                    Text(
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(Icons.mail, color: Colors.teal),
+                    title: Text(
                       'lvalandrorocha@hotmail.com',
                       style: TextStyle(
                         fontFamily: 'SourceSans',
                         color: Colors.teal.shade900,
                         fontSize: 20.0,
                       ),
-                    )
-                  ],
-                ),
-              )
+                    ),
+                  )),
             ],
           ),
         ),
